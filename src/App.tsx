e1d789e3e1371d6a3edecc332a1bdb0d1f1bfe4d
Dejv87 <david.novotny1987@gmail.com>
2025-03-28 18:44:47 +0100

    update

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>XTB Portfolio Tracker</h1>
+      <p>Vítej v aplikaci pro sledování investičního portfolia.</p>
+    </div>
+  );
+}
