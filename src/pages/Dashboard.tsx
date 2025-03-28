6cde5d15e7d7d37c35645b1c485ad196354fd1a3
Dejv87 <david.novotny1987@gmail.com>
2025-03-28 19:07:24 +0100

    update

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-2">Dashboard</h2>
+      <p>Zde uvidíš přehled portfolia, vývoj a další důležité statistiky.</p>
+    </div>
+  );
+}
